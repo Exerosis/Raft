61bc9208e4f94641b14f63ddf57e2963d4de07c1
Exerosis <exerosis@gmail.com>
2023-02-07 22:57:45 -0500

    ~ Starting to benchmark rabia.

--- node.go
+++ node.go
@@ -639,7 +639,7 @@ func StartRabia(config *Config, peers []Peer) *Rabia {
 		addresses[i] = url.Hostname()
 	}
 	fmt.Printf("Address: %s\n", address)
-	var pipes = make([]uint16, 2)
+	var pipes = make([]uint16, 16)
 	for i := range pipes {
 		pipes[i] = uint16(3000 + (i * 3))
 	}
